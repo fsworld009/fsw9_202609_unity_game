6c77333e6d242c7cedb4471abe6749278542f23c
fsworld009 <fsworld009@gmail.com>
2026-09-17 00:07:57 +0900

    敵登場する時のエフェクト追加

--- Assets/Scripts/TriggerPoint.cs
+++ Assets/Scripts/TriggerPoint.cs
@@ -4,6 +4,9 @@ public class TriggerPoint : MonoBehaviour
 {
 
     [SerializeField] private AudioClip triggerSfx;
+    [SerializeField] private GameObject spawnVfx;
+    [SerializeField] private Vector3 spawnVfxScale;
+    [SerializeField] private Vector3 spawnVfxRotation;
     private Transform spawnObjs;
     bool isTriggered;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -20,6 +23,13 @@ public class TriggerPoint : MonoBehaviour
         {
             child.gameObject.SetActive(false);
         }
+
+
+        // 召喚エフェクトの大きさ調整
+        foreach (Transform childVfx in spawnVfx.transform)
+        {
+            childVfx.localScale = spawnVfxScale;
+        }
     }
 
     // Update is called once per frame
@@ -30,6 +40,8 @@ public class TriggerPoint : MonoBehaviour
             GetComponent<AudioSource>().PlayOneShot(triggerSfx);
             foreach (Transform child in spawnObjs)
             {
+                GameObject vfx = Instantiate(spawnVfx, child.position, Quaternion.Euler(spawnVfxRotation));
+                Destroy(vfx, 0.5f);
                 child.gameObject.SetActive(true);
             }
             isTriggered = true;
